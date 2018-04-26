fe723b818719810c2590fb73ee6f727f90957560
Cody Fayolle <cfayolle@umich.edu>
2018-04-26 14:21:48 -0500

    we are doing things

--- client/components/tick.js
+++ client/components/tick.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import MIDISounds from 'midi-sounds-react';
 import {connect} from 'react-redux'
+import socket from '../socket';
 
 class Tick extends Component{
   constructor(props){
@@ -36,6 +37,7 @@ class Tick extends Component{
   }
 
   componentDidUpdate(){
+    socket.emit('newSound', this.props.grid)
     this.fillBeat()
   }
 
@@ -54,22 +56,22 @@ class Tick extends Component{
       var drums = [];
       var instrus = []
       if (this.state.tick[i]) {drums.push(this.state.drumTick)}
-      if (this.props.grid[0][0]) {drums.push(this.state.drumSound1)}
-      if (this.props.grid[0][1]) {drums.push(this.state.drumSound2)}
-      if (this.props.grid[0][2]) {drums.push(this.state.drumSound3)}
-      if (this.props.grid[0][3]) {drums.push(this.state.drumSound4)}
-      if (this.props.grid[1][0]) {drums.push(this.state.drumSound5)}
-      if (this.props.grid[1][1]) {drums.push(this.state.drumSound6)}
-      if (this.props.grid[1][2]) {drums.push(this.state.drumSound7)}
-      if (this.props.grid[1][3]) {drums.push(this.state.drumSound8)}
-      if (this.props.grid[2][0]) {drums.push(this.state.drumSound9)}
-      if (this.props.grid[2][1]) {drums.push(this.state.drumSound10)}
-      if (this.props.grid[2][2]) {drums.push(this.state.drumSound11)}
-      if (this.props.grid[2][3]) {drums.push(this.state.drumSound12)}
-      if (this.props.grid[3][0]) {drums.push(this.state.drumSound13)}
-      if (this.props.grid[3][1]) {drums.push(this.state.drumSound14)}
-      if (this.props.grid[3][2]) {drums.push(this.state.drumSound15)}
-      if (this.props.grid[3][3]) {drums.push(this.state.drumSound16)}
+      if (this.props.grid[0][0] || (this.incoming && this.incoming[0][0])) {drums.push(this.state.drumSound1)}
+      if (this.props.grid[0][1] || (this.incoming && this.incoming[0][1])) {drums.push(this.state.drumSound2)}
+      if (this.props.grid[0][2] || (this.incoming && this.incoming[0][2])) {drums.push(this.state.drumSound3)}
+      if (this.props.grid[0][3] || (this.incoming && this.incoming[0][3])) {drums.push(this.state.drumSound4)}
+      if (this.props.grid[1][0] || (this.incoming && this.incoming[1][0])) {drums.push(this.state.drumSound5)}
+      if (this.props.grid[1][1] || (this.incoming && this.incoming[1][1])) {drums.push(this.state.drumSound6)}
+      if (this.props.grid[1][2] || (this.incoming && this.incoming[1][2])) {drums.push(this.state.drumSound7)}
+      if (this.props.grid[1][3] || (this.incoming && this.incoming[1][3])) {drums.push(this.state.drumSound8)}
+      if (this.props.grid[2][0] || (this.incoming && this.incoming[2][0])) {drums.push(this.state.drumSound9)}
+      if (this.props.grid[2][1] || (this.incoming && this.incoming[2][1])) {drums.push(this.state.drumSound10)}
+      if (this.props.grid[2][2] || (this.incoming && this.incoming[2][2])) {drums.push(this.state.drumSound11)}
+      if (this.props.grid[2][3] || (this.incoming && this.incoming[2][3])) {drums.push(this.state.drumSound12)}
+      if (this.props.grid[3][0] || (this.incoming && this.incoming[3][0])) {drums.push(this.state.drumSound13)}
+      if (this.props.grid[3][1] || (this.incoming && this.incoming[3][1])) {drums.push(this.state.drumSound14)}
+      if (this.props.grid[3][2] || (this.incoming && this.incoming[3][2])) {drums.push(this.state.drumSound15)}
+      if (this.props.grid[3][3] || (this.incoming && this.incoming[3][3])) {drums.push(this.state.drumSound16)}
       var beat = [drums, instrus]
       this.beats[i] = beat
     }
@@ -83,7 +85,10 @@ class Tick extends Component{
   }
 
   render(){
-    var cool = this
+    socket.on('newSound', (grid) => {
+      this.incoming = grid
+      this.fillBeat()
+    })
     return(
       <div>
         <button onClick={this.playLoop}>Play loop</button>
--- server/socket/index.js
+++ server/socket/index.js
@@ -5,6 +5,11 @@ module.exports = (io) => {
     if (!play) {
       socket.emit('play')
     }
+
+    socket.on('newSound', (grid) => {
+      socket.broadcast.emit('newSound', grid)
+    })
+
     socket.on('disconnect', () => {
       console.log(`Connection ${socket.id} has left the building`)
     })
